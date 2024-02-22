4f845277b495947f9c0e3745607d4efdaf3ef08c
wamujlb <shamil.mammadof@gmail.com>
2024-02-22 17:39:22 +0100

    added custom menu items

---
+++ src-tauri/src/menu.rs
@@ -0,0 +1,18 @@
+use tauri::{CustomMenuItem, Menu, MenuItem, Submenu};
+
+pub fn app_menu() -> Menu {
+    let settings_menu_item = CustomMenuItem::new("settings".to_string(), "Settings");
+    let quit_menu_item = CustomMenuItem::new("quit".to_string(), "Quit");
+    let submenu = Submenu::new(
+        "Floating Clock",
+        Menu::new()
+            .add_item(settings_menu_item)
+            .add_item(quit_menu_item),
+    );
+    let menu = Menu::new()
+        .add_native_item(MenuItem::Copy)
+        .add_item(CustomMenuItem::new("hide", "Hide"))
+        .add_submenu(submenu);
+
+    menu
+}
